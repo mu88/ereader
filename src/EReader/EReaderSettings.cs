70e5f571e0032d91c4aaff106bbd403688f7f54f
mu88 <4560672+mu88@users.noreply.github.com>
2021-05-02 15:46:59 +0200

    Add implementation

---
+++ src/EReader/EReaderSettings.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable UnusedAutoPropertyAccessor.Global - reviewed mu88: will be handled in Startup by the runtime
+namespace EReader
+{
+    public class EReaderSettings
+    {
+        public string WebDavUrl { get; set; }
+        
+        public string User { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
---
+++ src/EReader/Pages/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using WebDav;
+
+namespace EReader.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IWebDavClient _webDavClient;
+        private readonly EReaderSettings _settings;
+
+        public IndexModel(HttpClient httpClient, IOptionsMonitor<EReaderSettings> settings)
+        {
+            _settings = settings.CurrentValue;
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("BASIC",
+                                              Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_settings.User}:{_settings.Password}")));
+            _webDavClient = new WebDavClient(httpClient);
+        }
+
+        public List<File> Files { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Files = await GetFilesAsync();
+        }
+
+        public async Task<IActionResult> OnGetCustomAsync(int id)
+        {
+            var file = (await GetFilesAsync()).ElementAt(id);
+            var response = await _webDavClient.GetRawFile(file.Uri);
+            return File(response.Stream, "application/octet-stream", file.Name);
+        }
+
+        private async Task<List<File>> GetFilesAsync()
+        {
+            var results = new List<File>();
+
+            var response = await _webDavClient.Propfind(_settings.WebDavUrl);
+            if (response.IsSuccessful)
+            {
+                var fileUris = response.Resources.Skip(1).Select(x => x.Uri);
+
+                var webDavUri = new Uri(_settings.WebDavUrl);
+                var host = webDavUri.Host;
+                var scheme = webDavUri.Scheme;
+                var port = webDavUri.Port;
+
+                results = fileUris.Select(x => new File { Uri = new Uri(new UriBuilder(scheme, host, port).Uri, x) }).ToList();
+            }
+
+            return results;
+        }
+    }
+
+    public class File
+    {
+        public string Name => Uri?.Segments?.Last() ?? string.Empty;
+
+        public Uri Uri { get; init; }
+    }
+}

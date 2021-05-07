107511a010b74e5ecf658e881c6ab5f42fc5c29f
mu88 <4560672+mu88@users.noreply.github.com>
2021-05-07 12:10:13 +0200

    Modify Docker deployment

--- src/EReader/Program.cs
+++ src/EReader/Program.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using EReader.Pages;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +16,27 @@ namespace EReader
     {
         public static void Main(string[] args)
         {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                System.IO.File.Copy("mu88_root_CA.crt", "/usr/local/share/ca-certificates/mu88_root_CA.crt");
+                
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "update-ca-certificates",
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = false,
+                    }
+                };
+                process.Start();
+                var standardOutput = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                Console.WriteLine(standardOutput);
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,7 +45,7 @@ namespace EReader
                 ConfigureAppConfiguration((_, config) => { config.AddEnvironmentVariables(); })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>().UseUrls("http://localhost:5202/");
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
